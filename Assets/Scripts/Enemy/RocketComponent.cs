b9d7e601b7c0ddd66b9ab8d5dfd2289111b3d874
Metin-Z <metinzehiroglu34@gmail.com>
2022-12-21 15:35:31 +0300

    line

--- Assets/Scripts/Enemy/RocketComponent.cs
+++ Assets/Scripts/Enemy/RocketComponent.cs
@@ -12,16 +12,18 @@ public class RocketComponent : MonoBehaviour
     [SerializeField] private BoxCollider boxCollider;
     [SerializeField] private MeshRenderer meshRenderer;
     [SerializeField] private Rigidbody rb;
+
     [SerializeField] private LineRenderer line;
+    [SerializeField] private Vector3 playerPos;
+    //[SerializeField] private int lineSegment =10;
 
     void Start()
     {
         transform.LookAt(PlayerController.Instance.transform);
-        Vector3 shootPos = (PlayerController.Instance.transform.position - transform.position).normalized;
-        rb.AddForce(shootPos * mySpeed);
-        Physics.Raycast(transform.position, PlayerController.Instance.transform.position);
-
-       // line.GetPosition(1) = Vector3
+        //Vector3 shootPos = (PlayerController.Instance.transform.position - transform.position).normalized;
+        //rb.AddForce(shootPos * mySpeed);
+        playerPos = PlayerController.Instance.transform.position;
+        //line.positionCount = lineSegment;
     }
     private void OnCollisionEnter(Collision collision)
     {
@@ -41,7 +43,33 @@ public class RocketComponent : MonoBehaviour
         Destroy(gameObject, 2);
         boxCollider.enabled = false;
         meshRenderer.enabled = false;
+        line.enabled = false;
         rb.isKinematic = true;
     }
+    private void Update()
+    {
+        //Visualize(transform.position);
+        transform.position = Vector3.MoveTowards(transform.position, PlayerController.Instance.transform.position, 14 * Time.deltaTime);
+        line.SetPosition(0,new Vector3(0,0,0));
+        line.SetPosition(1, new Vector3(0, 0, playerPos.z * 4));
+    }
+    //void Visualize(Vector3 vo)
+    //{
+    //    for (int i = 0; i < lineSegment; i++)
+    //    {
+    //        Vector3 pos = CalculatorPosInTime(vo, i / (float)lineSegment);
+    //        line.SetPosition(i, pos);
+    //    }
+    //}
+    //Vector3 CalculatorPosInTime(Vector3 vo,float time)
+    //{
+    //    Vector3 Vxz = vo;
+    //    vo.y = 0f;
 
+    //    Vector3 result = PlayerController.Instance.transform.position + vo * time;
+    //    float sY = (-0.5f * Mathf.Abs(Physics.gravity.y) * (time * time)) + (vo.y * time) + PlayerController.Instance.transform.position.y;
+    //    result.y = sY;
+
+    //    return result;
+    //}
 }
